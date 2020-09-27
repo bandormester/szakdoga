c8631409157933277b4b20ffaf50e0c0447417a8
Hajas András <bandi.14@hotmail.com>
2020-09-27 20:50:08 +0200

    Init

---
+++ Spring/szakdogaservice/szakdogaservice/src/main/java/hu/szurdok/szakdogaservice/SzakdogaserviceApplication.java
@@ -0,0 +1,13 @@
+package hu.szurdok.szakdogaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzakdogaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzakdogaserviceApplication.class, args);
+	}
+
+}
---
+++ Spring/szakdogaservice/szakdogaservice/src/test/java/hu/szurdok/szakdogaservice/SzakdogaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.szurdok.szakdogaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzakdogaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
